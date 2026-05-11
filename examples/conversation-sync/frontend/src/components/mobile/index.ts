2604b68764e1d796b8c39d70105d4b760a410717
Samuel Gbafa <skgbafa@gmail.com>
2026-05-10 20:53:13 -0400

    feat(listen): add mobile screens + bottom-nav shell per Listen Hi-Fi mobile artboards

---
+++ examples/conversation-sync/frontend/src/components/mobile/index.ts
@@ -0,0 +1,8 @@
+export { MobileShell } from "./MobileShell";
+export type { MobileTab } from "./MobileShell";
+export { MobileInbox } from "./MobileInbox";
+export type { MobileInboxItem } from "./MobileInbox";
+export { MobileDetail } from "./MobileDetail";
+export type { MobileDetailData, MobileDetailSentence } from "./MobileDetail";
+export { MobileChat } from "./MobileChat";
+export type { MobileChatMessage } from "./MobileChat";
